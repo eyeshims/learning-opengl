85a07140a592397cca6cd1de0abd5aa4dc23c124
eyeshims <eyeshims@gmail.com>
2015-11-04 18:14:32 +1300

    Create window glut on start up

--- Learning-OpenGL/main.cpp
+++ Learning-OpenGL/main.cpp
@@ -1,4 +1,16 @@
+#include "Dependencies\glew\glew.h"
+#include "Dependencies\freeglut\freeglut.h"
+#include <iostream>
+
 int main(int argc, char **argv)
 {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(500, 500);
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("OpenGL First Windw");
+
+    glutMainLoop();
+
     return 0;
 }
