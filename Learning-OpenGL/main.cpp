e7f634a6bdc67677a599cedee1dc287e66dd3dac
eyeshims <eyeshims@gmail.com>
2015-11-05 14:51:35 +1300

    Add helper classes for rendering vertices

--- Learning-OpenGL/main.cpp
+++ Learning-OpenGL/main.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
@@ -5,9 +6,11 @@
 #include<vector>
 
 #include "Shader_Loader.h"
+#include "GameModels.h"
 
 using namespace Core;
 
+Models::GameModels *gameModels;
 GLuint program;
 
 void renderScene(void)
@@ -15,22 +18,33 @@ void renderScene(void)
     // Clear OpenGL's color and depth buffers.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Set the color buffer to red.
-    glClearColor(1.0, 0.0, 0.0, 1.0);
+    // Set the color buffer to dark turquoise.
+    glClearColor(0.0, 0.3, 0.3, 1.0);
+
+    glBindVertexArray(gameModels->GetModel("triangle1"));
 
     // Use the created program.
     glUseProgram(program);
 
-    // Draw 3 vertices as triangkes.
+    // Draw 3 vertices as triangles.
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // Move the back buffer to the front.
     glutSwapBuffers();
 }
 
+void closeCallback()
+{
+    std::cout << "GLUT:\t Finished" << std::endl;
+    glutLeaveMainLoop();
+}
+
 void Init()
 {
     glEnable(GL_DEPTH_TEST);
+
+    gameModels = new Models::GameModels();
+    gameModels->CreateTriangleModel("triangle1");
 
     // Load and compile shaders.
     Core::Shader_Loader shaderLoader;
@@ -51,6 +65,8 @@ int main(int argc, char **argv)
     glutInitWindowPosition(50, 50);
     glutInitWindowSize(800, 800);
     glutCreateWindow("OpenGL 3.1");
+    glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+
     glewInit();
 
     // Check which if OpenGL version 3.1 is supported on this machine.
@@ -65,7 +81,11 @@ int main(int argc, char **argv)
 
     // Register a callback that renders the scene.
     glutDisplayFunc(renderScene);
+    glutCloseFunc(closeCallback);
+    
     glutMainLoop();
+
+    delete gameModels;
     glDeleteProgram(program);
 
     return 0;
